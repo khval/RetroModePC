1265b6f98aa3b5db152be294bb655215ecb7b9a6
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-01-14 19:21:15 +0100

    Stdafx.h is included into all files
    sys_* is for OS native memory alloc.

--- stdafx.h
+++ stdafx.h
@@ -7,11 +7,20 @@
 
 #define _CRT_NONSTDC_NO_DEPRECATE
 
+#ifndef __linux__
 #include "targetver.h"
+#endif
 
 #include <stdio.h>
+
+#ifndef __linux__
 #include <tchar.h>
+#endif
 
-
-
+#if defined(__linux__)
+#define BOOL bool
+#define sys_alloc(size) malloc(size)
+#define sys_alloc_clear(size) calloc(1,size)
+#define sys_free free
+#endif
 // TODO: reference additional headers your program requires here
